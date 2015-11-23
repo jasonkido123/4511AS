1104feda71a170db5b61e5c12392bce93ef4ecde
Jason <jjshum678@livemail.tw>
2015-11-23 18:43:44 +0800

    1843

--- src/java/ict/servlet/ItemController.java
+++ src/java/ict/servlet/ItemController.java
@@ -1,5 +1,6 @@
 package ict.servlet;
 
+import ict.db.ItemDb;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -10,30 +11,29 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ItemController", urlPatterns = {"/additem"})
 public class ItemController extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ItemController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ItemController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    private ItemDb db;
+    
+    public void init (){
+        String dbUser = this.getServletContext().getInitParameter("dbUser");
+        String dbPassword = this.getServletContext().getInitParameter("dbPassword");
+        String dbUrl = this.getServletContext().getInitParameter("dbUrl");
+        db = new ItemDb(dbUrl, dbUser, dbPassword);
+    }
+    
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (!isAuthenticated(request)
+                && !("authenticate".equals(action))) {
+            doLogin(request, response);
+            return;
+        }
+        if ("authenticate".equals(action)) {
+            doAuthenticate(request, response);
+        } else if ("logout".equals(action)) {
+            doLogout(request, response);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_IMPLEMENTED);
         }
     }
 }
